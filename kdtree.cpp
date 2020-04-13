2843bc8ac45d9ed1b7ba0da94fe04eebc05b3552
Joe-08 <joe_andre0710@hotmail.com>
2020-04-13 15:12:06 -0500

    feat(kdtree): create kdtree class
    Create new class KdTree with templates and functions insert, search and
    print.

--- kdtree.cpp
+++ kdtree.cpp
@@ -39,6 +39,94 @@ class KdNode {
     }
 };
 
+template <typename T>
+class KdTree {
+public:
+  typedef vector<T> container_t; 
+  
+  KdNode<T> *root;
+  int k;
+  
+  KdTree(int _k):root(nullptr), k(_k) {
+  }
+
+  ~KdTree() {
+  }
+
+  bool cmpNode(KdNode<T> *a, KdNode<T> *b) {
+    return (a->point == b->point);
+  }
+
+  bool searchR(KdNode<T> *node, KdNode<T> *_root) {
+    if (_root == nullptr) return false;
+    if (cmpNode(_root, node)) return true;
+
+    int dim = _root->level % k;
+
+    if (node->point[dim] < _root->point[dim])
+      return searchR(node, _root->left);
+
+    return searchR(node, _root->right);
+  }
+
+  bool search(container_t _point) {
+    KdNode<T> *node = new KdNode<T>(_point);
+    
+    return searchR(node, root);
+  }
+
+  bool insertR(KdNode<T> *node, KdNode<T> *_root) {
+    int dim = _root->level % k;
+    node->level++;
+    
+    if (node->point[dim] < _root->point[dim]) {
+      if (_root->left == nullptr) {
+        _root->left = node;
+        return true;
+      } else
+        insertR(node, _root->left);
+    } else {
+      if (_root->right == nullptr) {
+        _root->right = node;
+        return true;
+      } else 
+        insertR(node, _root->right);
+    }
+    return false;
+  }
+
+  bool insert(container_t _point) {
+    if (root == nullptr) {
+      root = new KdNode<T>(_point);
+      return true;
+    } 
+    
+    KdNode<T> *tmp = new KdNode<T>(_point);
+      
+    return insertR(tmp, root);
+  }
+
+  void printTreeR(KdNode<T> *root, int space) {
+    if (root == nullptr) return;
+    
+    space += 10;
+    
+    printTreeR(root->right, space);
+    
+    cout << endl;
+    for (int i = 5; i < space; i++)
+      cout << " ";
+    cout << root << endl;
+
+    printTreeR(root->left, space);
+
+  }
+
+  void printTree() {
+    printTreeR(root, 0);
+  }
+};
+
 int main() 
 {
 
